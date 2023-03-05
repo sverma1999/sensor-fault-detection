1f9cc3cf03eb928b2b25a66c194d10e701590402
Shubham Verma <sverma1@unb.ca>
2023-03-05 19:39:44 -0400

    Completed data ingestion part

--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -19,6 +19,7 @@ from sensor.entity.artifact_entity import (
 )
 
 from sensor.exception import SensorException
+from sensor.components.data_ingestion import DataIngestion
 
 from sensor.logger import logging
 import sys, os
@@ -28,9 +29,9 @@ import sys, os
 class TrainingPipeline:
     def __init__(self):
 
-        training_pipeline_config = TrainingPipelineConfig()
-        self.data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
-        self.training_pipeline_config = training_pipeline_config
+        self.training_pipeline_config = TrainingPipelineConfig()
+        
+        #self.training_pipeline_config = training_pipeline_config
 
 
         # self.data_validation_config = DataValidationConfig()
@@ -46,8 +47,12 @@ class TrainingPipeline:
 
     def start_data_ingestion(self) -> DataIngestionArtifact:
         try:
+            self.data_ingestion_config = DataIngestionConfig(training_pipeline_config=self.training_pipeline_config)
             logging.info("Entered the start_data_ingestion method of TrainPipeline class")
-            logging.info("Getting the data from mongodb")
+            data_ingestion = DataIngestion(data_ingestion_config=self.data_ingestion_config)
+            data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+            logging.info(f"Data ingestion completed and artifact: {data_ingestion_artifact}")
+            return data_ingestion_artifact
         except Exception as e:
             raise SensorException(e, sys)
         
