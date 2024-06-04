1c8ed568e29c2b043cbf045a7c15c2ebdd815e9d
Shubham Verma <sverma1@unb.ca>
2024-06-04 00:53:29 -0300

    Added terraform code and its documentation

--- Infrastructure/module.tf
+++ Infrastructure/module.tf
@@ -1,23 +1,35 @@
 terraform {
   backend "s3" {
-    bucket = "sensor-tf-state"
-    key    = "tf_state"
-    region = "us-east-1"
+    bucket  = "sfd-tf-state-bucket"
+    key     = "key/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
   }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  # region = lookup(env, "AWS_REGION")
 }
 
 module "sensor_ec2" {
   source = "./sensor_ec2"
 }
 
-module "sensor_model" {
-  source = "./sensor_model_bucket"
-}
+# module "sensor_model" {
+#   source = "./sensor_model_bucket"
+#   providers = {
+#     aws = aws
+#   }
+# }
 
 module "sensor_ecr" {
   source = "./sensor_ecr"
 }
 
-module "sensor_pred_data" {
-  source = "./sensor_pred_data_bucket"
-}
+# module "sensor_pred_data" {
+#   source = "./sensor_pred_data_bucket"
+#   providers = {
+#     aws = aws
+#   }
+# }
